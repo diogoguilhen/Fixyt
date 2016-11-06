ee48b042b933d92295489bc705fe4f2e72c3b570
Diogo Henrique S Guilhen <diogoguilhen@users.noreply.github.com>
2016-11-06 17:04:54 -0200

    teste Android Studio Enter a mais na Registrar_1

--- app/src/main/java/fixyt/fixyt/Registrar_1.java
+++ app/src/main/java/fixyt/fixyt/Registrar_1.java
@@ -28,6 +28,7 @@ import java.util.Objects;
 
 public class Registrar_1 extends AppCompatActivity implements OnClickListener{
 
+
     private Button botaoRegistrar;
     private EditText campoEmail;
     private EditText campoSenha;
