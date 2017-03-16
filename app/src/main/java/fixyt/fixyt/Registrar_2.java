d548a95fffce0ea0133920c163c4fd16c946da4f
Diogo Henrique S Guilhen <diogo.guilhen@gmail.com>
2017-03-16 00:18:14 -0300

    Cadastro completo e padronizado.

--- app/src/main/java/fixyt/fixyt/Registrar_2.java
+++ app/src/main/java/fixyt/fixyt/Registrar_2.java
@@ -21,7 +21,11 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 import com.google.firebase.auth.FirebaseAuthInvalidUserException;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import android.app.Activity;
 import android.os.Bundle;
