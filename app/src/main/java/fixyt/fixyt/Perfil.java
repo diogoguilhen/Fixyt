424e4629bdba0ce9ae92668d5566127b7a3f3092
Sergio <sergio.dsipos@gmail.com>
2017-03-20 01:11:44 -0300

    redefinição de Senha pronto

--- app/src/main/java/fixyt/fixyt/Perfil.java
+++ app/src/main/java/fixyt/fixyt/Perfil.java
@@ -2,12 +2,18 @@ package fixyt.fixyt;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -15,7 +21,9 @@ public class Perfil extends AppCompatActivity implements View.OnClickListener {
 
     private FirebaseAuth firebaseAuth;
     private TextView emailUsuario;
+    private EditText campoSenha;
     private Button logOut;
+    private Button redefSenha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,13 +37,20 @@ public class Perfil extends AppCompatActivity implements View.OnClickListener {
             startActivity(new Intent(this, PreLogin.class));
         }
 
-        FirebaseUser usuario = firebaseAuth.getCurrentUser();
+        FirebaseUser usuario = firebaseAuth.getInstance().getCurrentUser();
+
+
+
 
         emailUsuario = (TextView) findViewById(R.id.textUsuarioEmail);
-        emailUsuario.setText("Bem-Vindo " + usuario.getEmail());
+        emailUsuario.setText("Bem-Vindo " + usuario.getEmail() + "!!");
+        campoSenha = (EditText) findViewById(R.id.novaSenha);
+
         logOut = (Button) findViewById(R.id.botaoLogout);
+        redefSenha = (Button) findViewById(R.id.botaoRedefinirSenha);
 
         logOut.setOnClickListener(this);
+        redefSenha.setOnClickListener(this);
 
     }
 
@@ -46,5 +61,23 @@ public class Perfil extends AppCompatActivity implements View.OnClickListener {
             finish();
             startActivity(new Intent(this, PreLogin.class));
         }
+        if(v == redefSenha){
+            FirebaseUser usuario = firebaseAuth.getInstance().getCurrentUser();
+
+            String newPassword = campoSenha.getText().toString();
+
+
+            usuario.updatePassword(newPassword)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(Perfil.this, "Senha Atualizada com Sucesso!", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
     }
 }
+
+
